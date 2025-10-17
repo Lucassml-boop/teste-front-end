48360a7dbc5dab969a2f2e8a5acc2611536d7e7e
Lucassml-boop <lucas.samuel23borges@gmail.com>
2025-10-16 21:50:38 -0300

    feat: setup React Router and create HomePage layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import App from "./App";
+import HomePage from "./pages/HomePage";
 
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById("root");
+if (!container) {
+  throw new Error("Root element not found");
+}
+
+const root = createRoot(container);
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </Router>
+  </StrictMode>
+);
